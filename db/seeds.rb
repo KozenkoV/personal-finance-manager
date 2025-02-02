920c3b61b4dc1aa49bc9ef3d27f680d5e8c35ccd
Vasyl <vasyl.hladkyi@spirent.com>
2025-02-02 16:06:12 +0200

    Feature: User autorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@ CATEGORY_3 = Category.create!(name: 'Відпочинок', description: 'Вит
 Transaction.create!(transaction_type: 'Витрата', total: 45.00, description: 'Акрополь', category: CATEGORY_2)
 Transaction.create!(transaction_type: 'Продукти харчування', total: 200.00, description: 'Продукти', category: CATEGORY_1)
 Transaction.create!(transaction_type: 'Витрата', total: 50.00, description: 'Обід в Коморі', category: CATEGORY_2)
+
+User.create!(name: 'Admin', email: 'admin@user.com', password: '123456', password_confirmation: '123456')

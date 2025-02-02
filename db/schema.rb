c5036fb9266790b3cbeff0927d9a4eaf3cdef22b
Kozenko Vitalii <vitalii.kozenko-pi2103@kep.nung.edu.ua>
2025-02-02 14:41:52 +0000

    Feature: Implement transactions CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_27_122906) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_29_204751) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", default: "", null: false
@@ -18,4 +18,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_27_122906) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "transaction_type", null: false
+    t.text "description", default: "", null: false
+    t.float "total", default: 0.0, null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+  end
+
+  add_foreign_key "transactions", "categories"
 end
